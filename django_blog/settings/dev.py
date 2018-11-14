5cbdfb7c0bfa6b3f186987beb5b66b60e987e480
Kira <kira.anleu@hotmail.com>
2018-11-14 12:34:02 +0000

    Upgrating Django

--- django_blog/settings/dev.py
+++ django_blog/settings/dev.py
@@ -2,6 +2,7 @@ from .base import *
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
