9c4113ad75de3403e24eecb8cfeafffcc5fa9916
Kira <kira.anleu@hotmail.com>
2018-11-14 12:40:48 +0000

    Added allowed Hosts

--- django_blog/settings/base.py
+++ django_blog/settings/base.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-kiranleu.c9users.io']
+ALLOWED_HOSTS = ['django-blog-kiranleu.c9users.io',
+                 'kira-django-blogapp.herokuapp.com'    ]
 
 
 # Application definition
